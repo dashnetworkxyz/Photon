e55e07f3c72330f1139f8444b721c0b6ff02e8af
MasterDash5 <constant4337@molecularmail.com>
2025-09-13 20:13:53 -0600

    return builder while building

--- src/xyz/dashnetwork/photon/npc/PhotonBuilder.java
+++ src/xyz/dashnetwork/photon/npc/PhotonBuilder.java
@@ -26,78 +26,98 @@ public class PhotonBuilder {
     private boolean hideNametag;
     private boolean autoView = true;
 
-    public void animator(Animator animator) {
+    public PhotonBuilder animator(Animator animator) {
         this.animator = (npc) -> animator;
+        return this;
     }
 
-    public void mover(Mover mover) {
+    public PhotonBuilder mover(Mover mover) {
         this.mover = (npc) -> mover;
+        return this;
     }
 
-    public void moveController() {
+    public PhotonBuilder moveController() {
         this.mover = MoveController::new;
+        return this;
     }
 
-    public void onAttack(BiConsumer<NPC, Player> consumer) {
+    public PhotonBuilder onAttack(BiConsumer<NPC, Player> consumer) {
         if (attackCallback == null)
             attackCallback = consumer;
         else
             attackCallback = attackCallback.andThen(consumer);
+
+        return this;
     }
 
-    public void onInteract(BiConsumer<NPC, Player> consumer) {
+    public PhotonBuilder onInteract(BiConsumer<NPC, Player> consumer) {
         if (interactCallback == null)
             interactCallback = consumer;
         else
             interactCallback = interactCallback.andThen(consumer);
+
+        return this;
     }
 
-    public void onClick(BiConsumer<NPC, Player> consumer) {
+    public PhotonBuilder onClick(BiConsumer<NPC, Player> consumer) {
         onAttack(consumer);
         onInteract(consumer);
+
+        return this;
     }
 
-    public void location(Location location) {
+    public PhotonBuilder location(Location location) {
         this.location = location;
+        return this;
     }
 
-    public void location(World world, double x, double y, double z, float yaw, float pitch) {
+    public PhotonBuilder location(World world, double x, double y, double z, float yaw, float pitch) {
         this.location = new Location(world, x, y, z, yaw, pitch);
+        return this;
     }
 
-    public void location(World world, double x, double y, double z) {
+    public PhotonBuilder location(World world, double x, double y, double z) {
         this.location = new Location(world, x, y, z);
+        return this;
     }
 
-    public void profile(String name, UUID uuid) {
+    public PhotonBuilder profile(String name, UUID uuid) {
         this.profile = new GameProfile(uuid, name);
+        return this;
     }
 
-    public void profile(String name) {
+    public PhotonBuilder profile(String name) {
         this.profile = new GameProfile(UUID.randomUUID(), name);
+        return this;
     }
 
-    public void skin(String json, String signature) {
+    public PhotonBuilder skin(String json, String signature) {
         this.profile.getProperties().put("textures", new Property("textures", json, signature));
+        return this;
     }
 
-    public void skin(UUID account) {
+    public PhotonBuilder skin(UUID account) {
         this.profile.getProperties().put("textures",
                 MinecraftServer.getServer().aD().fillProfileProperties(new GameProfile(account, ""), true)
                         .getProperties().get("textures").iterator().next()
         );
+
+        return this;
     }
 
-    public void skinAuto() {
+    public PhotonBuilder skinAuto() {
         this.profile = MinecraftServer.getServer().aD().fillProfileProperties(profile, true);
+        return this;
     }
 
-    public void disableAutoView() {
+    public PhotonBuilder disableAutoView() {
         this.autoView = false;
+        return this;
     }
 
-    public void hideNametag() {
+    public PhotonBuilder hideNametag() {
         this.hideNametag = true;
+        return this;
     }
 
     public NPC build() {
